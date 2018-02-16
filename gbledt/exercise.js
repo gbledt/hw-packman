6210d8d17e729d098de7e7132e69b3fe83a7bd4a
gbledt <gbledt@mit.edu>
2018-02-16 10:32:46 -0500

    PacMan Hw commit

--- gbledt/exercise.js
+++ gbledt/exercise.js
@@ -20,22 +20,41 @@ exercise.updatePosition = function() {
     // increment exercise.pos.x by increment 
     // now set image position using img1.style.left 
     // remember images positions are "xxx.px"
-
+    exercise.pos.x = exercise.pos.x + exercise.increment;
+    exercise.img1.style.left = exercise.pos.x + "px";
 
 };
 exercise.chooseImage = function() {
     // choose between all 4 images
     if (exercise.increment > 0) {
         if (exercise.flag === 1) {
+            // display image 1 
+            exercise.img1.src = "PacMan1.png";
+
+            // set flag to 0
+            exercise.flag = 0;
 
         } else {
+            // display image 2 
+            exercise.img1.src = "PacMan2.png";
 
+            // set flag to 1
+            exercise.flag = 1;
         }
     } else if (exercise.increment < 0) {
         if (exercise.flag === 1) {
+            // display image 3 
+            exercise.img1.src = "PacMan3.png";
+
+            // set flag to 0
+            exercise.flag = 0;
 
         } else {
+            // display image 4 
+            exercise.img1.src = "PacMan4.png";
 
+            // set flag to 1
+            exercise.flag = 1;
         }
     }
 };
@@ -43,5 +62,10 @@ exercise.checkWallCollision = function() {
     // reset the direction of motion if wall is hit
     // you need to take into account image width
 
+    // check if we have run into the wall
+    if (exercise.pos.x + exercise.img1.clientWidth >= 800 || exercise.pos.x == 0) {
+        // switch the direction
+        exercise.increment = -exercise.increment
+    }
 
 };
